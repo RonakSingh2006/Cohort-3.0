0cfc573188b0f5b18bf794583a8d07f8d07d945e
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-11-12 21:11:07 +0530

    initialized a react project with tailwind config classes

---
+++ WEB DEV/TailwindCss/tailwind-class-1/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return <>
+    {/* <div style={{display : "flex" , justifyContent : "center"}}> Raw css */}
+    <div className="flex justify-center">
+      <div>Child 1</div>
+      <div>Child 2</div>
+      <div>Child 3</div>
+    </div>
+  </>
+}
+
+export default App
