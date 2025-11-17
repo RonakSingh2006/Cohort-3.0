b0371f713c54d21fa6d999c74c50f3cc8253508e
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-11-18 00:08:57 +0530

    learned interface and implemented it in class

--- WEB DEV/TypeScript/src/ronak.ts
+++ WEB DEV/TypeScript/src/ronak.ts
@@ -32,3 +32,46 @@ let aaditya : Person = {
 
 complexGreet(ronak);
 complexGreet(aaditya);
+
+
+interface People {
+  name : string,
+  age : number,
+  greet: ()=>string
+}
+
+
+
+let harkirat :People = {
+  name : "Harkirat",
+  age : 32,
+  greet : ()=>{return "Hello"}
+}
+
+console.log(harkirat.greet());
+
+
+// Same java interface
+
+class Human implements People{
+  name:string;
+  age:number
+  constructor(name: string , age: number){
+    this.name = name;
+    this.age = age;
+  }
+
+  greet = () => {
+    return `hello ${this.name}`
+  } 
+
+  help = ()=>{
+    return 5
+  }
+
+}
+
+
+let nh:Human = new Human("Ronak",19);
+
+console.log(nh.greet())
