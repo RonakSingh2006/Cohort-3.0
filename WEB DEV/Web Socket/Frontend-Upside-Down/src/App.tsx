91a39768ee6a434b7d98559e2768c10f60d65029
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-06 13:30:18 +0530

    changed name and write ws server

---
+++ WEB DEV/Web Socket/Frontend-Upside-Down/src/App.tsx
@@ -0,0 +1,17 @@
+function App(){
+  return <>
+    <div className="w-screen h-screen bg-gray-400">
+
+
+
+
+    </div>
+    <div className="h-14 w-screen bg-red-300 fixed bottom-2 left-0 flex justify-center items-center">
+      <div className="bg-red-900 w-1/2 h-12">
+
+      </div>
+    </div>
+  </>
+}
+
+export default App;
