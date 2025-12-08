427e13303cb94d113f70c77215e268aeab272ec9
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-09 00:33:27 +0530

    used relation in prisma

--- WEB DEV/Prisma/src/index.ts
+++ WEB DEV/Prisma/src/index.ts
@@ -1,13 +1,25 @@
 import { prisma } from './lib/prisma.js'
 
 async function main() {
-  await prisma.user.create({
-    data : {
-      usename : "RandomAccount",
-      password : "Random@3214",
-      age : 19
-    }
+  // await prisma.user.create({
+  //   data : {
+  //     usename : "RandomAccount",
+  //     password : "Random@3214",
+  //     age : 19
+  //   }
+  // })
+
+
+  const user = await prisma.user.findFirst({
+    where : {
+      id : 1
+    },
+    include : {
+      todos : true
+    } 
   })
+
+  console.log(user);
 }
 
 main();
