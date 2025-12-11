e95ff30cd065d79074b02c1b8f1a32927f246f41
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-11 09:23:38 +0530

    moved backend in nextJs using route.js

--- WEB DEV/NEXTJS/first-app/app/employees/page.tsx
+++ WEB DEV/NEXTJS/first-app/app/employees/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 async function getEmployees() {
-  const response = await axios.get('https://dummy.restapiexample.com/api/v1/employees');
+  const response = await axios.get('http://localhost:3000/api/v1/employees');
   console.log(response.data);
   return response.data;
 }
