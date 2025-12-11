f899dfcdd50b51963267b400247dcc47cf9c92a9
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-12-11 11:41:41 +0530

    initialized prisma and completed signup route

--- WEB DEV/NEXTJS/first-app/app/api/v1/signup/route.ts
+++ WEB DEV/NEXTJS/first-app/app/api/v1/signup/route.ts
@@ -1,10 +1,17 @@
 import { NextResponse } from "next/server";
 
+import {prisma} from "../../../lib/prisma"
+
 export async function POST(req : NextResponse){
 
   const {username,password} = await req.json();
 
-  console.log(username,password);
+  await prisma.user.create({
+    data : {
+      username,
+      password
+    }
+  })
 
   return NextResponse.json({
     message : "Signed Up"
