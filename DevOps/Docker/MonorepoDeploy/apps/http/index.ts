11f0cf8770a5e77bcde13df45ba56ded9fad22c3
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-01-08 01:01:22 +0530

    built a mono repo app

--- DevOps/Docker/MonorepoDeploy/apps/http/index.ts
+++ DevOps/Docker/MonorepoDeploy/apps/http/index.ts
@@ -3,8 +3,20 @@ import {prisma} from "db/client"
 
 const app = express();
 
-app.get("/",(req,res)=>{
-  res.send("Hello World");
+app.get("/",async (req,res)=>{
+
+  const users = await prisma.user.findMany();
+
+  res.send({users});
 });
+
+app.post("/",async (req,res)=>{
+  await prisma.user.create({
+    data : {
+      username : Math.random().toString(),
+      password : Math.random().toString()
+    }
+  })
+})
 
 app.listen(3000);
--- DevOps/Docker/MonorepoDeploy/apps/ws/index.ts
+++ DevOps/Docker/MonorepoDeploy/apps/ws/index.ts
@@ -1,13 +1,28 @@
 import {WebSocketServer} from "ws"
+import {prisma} from "db/client"
 
 const wss = new WebSocketServer({port : 8080});
 
-
-wss.on("connection",(socket)=>{
+wss.on("connection",async (socket)=>{
   console.log("Connected");
+
+  const user = await prisma.user.create({
+    data : {
+      username : Math.random().toString(),
+      password : Math.random().toString()
+    }
+  })
 
   socket.on("message",(data)=>{
     console.log(data);
+
+    prisma.todo.create({
+      data : {
+        task : data.toString(),
+        done : false,
+        userId : user.id
+      }
+    })
   })
 
   socket.send("Hello");
