806a4722a64fda13d8cb0580e09de37969f86da6
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-02-25 02:20:28 +0530

    added heavy task

--- DevOps/Scalling/scalling-exp/index.js
+++ DevOps/Scalling/scalling-exp/index.js
@@ -19,8 +19,14 @@ if(cluster.isPrimary){
 else{
   const app = express();
 
-  app.get("/",(req,res)=>{
-    res.send(`Hello world ${process.pid}`);
+  app.get("/:n",(req,res)=>{
+    const n = req.params.n;
+    let cnt = 0;
+    for(let i=0 ; i<n ; i++){
+      cnt += i;
+    }
+
+    res.send(`${cnt} Hello world ${process.pid}`);
   });
 
   app.listen(3000,console.log("Running localhost:3000"));
