593113877289682064bd4dc1eecf6dfe77bac6a9
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-05-29 19:22:24 +0530

    det desired capacity using aws-sdk

--- DevOps/AutoScalling_Orchestrator/index.ts
+++ DevOps/AutoScalling_Orchestrator/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import "dotenv/config";
+
+import { AutoScalingClient,SetDesiredCapacityCommand } from "@aws-sdk/client-auto-scaling";
+import express from "express"
+
+const client = new AutoScalingClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const command = new SetDesiredCapacityCommand({AutoScalingGroupName : "vscode-asg" , DesiredCapacity : 1})
+
+const data = await client.send(command);
+
+console.log(data);
