5ccf7712d2d49ab7da7ba7b1f203c3160bee69ff
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-06-24 00:38:49 +0530

    logging using winston in newrlic

--- DevOps/Observability/NewRelic/node-app/index.js
+++ DevOps/Observability/NewRelic/node-app/index.js
@@ -1,10 +1,34 @@
 import express from 'express';
 import 'newrelic';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
 
 
 const app = express();
 
 app.get('/',(req,res)=>{
+        logger.info('route hit');
+
+        if(Math.random() < 0.5){
+                logger.error('Randome Error');
+        }
+
         res.send("hello world");
 });
 
